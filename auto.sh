2ad2338bc648a1249d891c2763a4c7a3559849a2
lonewolfyx <877085893@qq.com>
2024-12-05 09:44:07 +0800

    auto

---
+++ auto.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ 
+# 创建日志文件路径
+log_file="log/log_$(date +"%Y%m%d").log"
+ 
+# 确保日志目录存在
+mkdir -p log
+ 
+# 执行 touch 命令并将结果追加到日志文件中
+# touch "$file_name" && e
+echo "$(date +"%Y-%m-%d %H:%M:%S") systemd[1]: Started Github Commit" >> "$log_file"
